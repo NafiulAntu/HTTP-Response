9b9e362412da503d072cae89054fd385b1df5e37
nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-02-23 12:13:38 +0600

    Add files via upload

---
+++ JS-3.js
@@ -0,0 +1,39 @@
+/* var gustList = ["Antu", "Nafiu", "Islam", "Ayman", "Saro"];
+
+if (gustList.includes(gustList)) {
+  alert("welcome!");
+} else {
+  alert("Sorry, maybe next time.");
+}
+
+var numberOfBottles = 99;
+while (numberOfBottles >= 0) {
+  var bottleWord = "bottle";
+  if (numberOfBottles === 1) {
+    bottleWord = "bottles";
+  }
+  console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+  console.log(numberOfBottles + " " + bottleWord + " of beer,");
+  console.log("Take one down, pass it around,");
+  numberOfBottles--;
+  console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
+*/
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("Fizzbuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+    count++;
+  }
+  console.log(output);
+}
