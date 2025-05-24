63fc9c9e78d1a5c49051bb04f056ed9f15eaf3c9
Nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-05-24 19:18:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+import express from "express";
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1> Nafiul Islam </h1>");
+});
+
+app.get("/me", (req, res) => {
+  res.send("<h1> About me </h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1> about you </h1>");
+});
+app.get("/you", (req, res) => {
+  res.send("<h1>  i need a slave </h1>");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
